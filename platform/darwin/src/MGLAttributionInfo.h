ae9d793d1c0ae5d0a5a6808653b3005dd16dbe60
Fabian Guerra <fabian.guerra@mapbox.com>
2017-11-21 16:32:05 -0500

    [ios, macos] Refactor snapshot attribution.

--- platform/darwin/src/MGLAttributionInfo.h
+++ platform/darwin/src/MGLAttributionInfo.h
@@ -8,6 +8,24 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
+ The attribution info is represented in the longest format available.
+ */
+typedef NS_ENUM(NSUInteger, MGLAttributionInfoStyle) {
+    /**
+     Specifies a short attribution info style.
+     */
+    MGLAttributionInfoStyleShort = 1,
+    /**
+     Specifies a medium attribution info style.
+     */
+    MGLAttributionInfoStyleMedium,
+    /**
+     Specifies a long attribution info style.
+     */
+    MGLAttributionInfoStyleLong
+};
+
+/**
  Information about an attribution statement, usually a copyright or trademark
  statement, associated with a map content source.
  */
@@ -58,6 +76,19 @@ MGL_EXPORT
     `nil`.
  */
 - (nullable NSURL *)feedbackURLAtCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate zoomLevel:(double)zoomLevel;
+
+/**
+ Returns a copy of the current `title` formatted accordingly to `style`.
+ 
+ Example: If `style` = `MGLAttributionInfoStyleShort` and
+ `MGLAttributionInfo.title` = `OpenStreetMap` then it will format
+ `MGLAttributionInfo.title` = `OSM`
+ 
+ @param style The attribution info style.
+ 
+ @return The `NSAttributedString` styled title.
+ */
+- (NSAttributedString *)titleWithStyle:(MGLAttributionInfoStyle)style;
 
 @end
 
